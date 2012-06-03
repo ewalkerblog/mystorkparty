642532d8ceed68d6e7f2c5fa2e3a58ad08225f54
Erin Walker <ewalker@ewalkerblog.co.za>
2012-06-03 18:48:49 +0200

    second_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystorkparty::Application.config.secret_token = '922ad5e70c94203520fc198b7678c046ac3141a4fda251842e3a417d1885fdcc0c6d2e1564982ce475c2a435b5762408e5c54323e2cb5834f2baacda6de1bde6'
