8beaf2f4a35490828e23c0675ea670f7e451bb09
Erin Walker <ewalker@ewalkerblog.co.za>
2012-06-05 16:33:52 +0200

    we can mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,25 @@ Mystorkparty::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
+  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options   = { :host => 'localhost:3000' }
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "ewalker@ewalkerblog.co.za",
+  :authentication => :login,
+  :user_name => "ewalker@ewalkerblog.co.za",
+  :password => "VXLC2fM",
+  }
   config.cache_classes = false
+  
+  
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
@@ -12,9 +30,6 @@ Mystorkparty::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
