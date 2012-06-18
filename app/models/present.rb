629d133ada4dbaa01b498750beb272b1c5d240b0
Erin Walker <ewalker@ewalkerblog.co.za>
2012-06-18 23:28:47 +0200

    added_validations

--- app/models/present.rb
+++ app/models/present.rb
@@ -2,4 +2,5 @@ class Present < ActiveRecord::Base
   set_inheritance_column "not_sti"
   attr_accessible :brand, :color, :quantity, :registry_id, :type
   belongs_to :registry
+  validates :brand, :color, :quantity, :type, presence: true
 end
--- app/models/registry.rb
+++ app/models/registry.rb
@@ -1,6 +1,9 @@
 class Registry < ActiveRecord::Base
   attr_accessible :due_date, :gender, :present_id, :theme, :user_id, :presents_attributes, :party_name, :name
   validates :user_id, presence: true
+  validates :name, :gender, :due_date, :theme, presence: true, uniqueness: true
+  VALID_PARTY_NAME_REGEX = /\A[\w+\-.]+\z/
+  validates :party_name, presence: true, uniqueness: true, format: { with: VALID_PARTY_NAME_REGEX }
   belongs_to :user
   has_many :presents
   accepts_nested_attributes_for :presents, allow_destroy: true
