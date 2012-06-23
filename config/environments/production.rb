f7dd36ad191e822b58bf9d261322c9fb5271cac5
Erin Walker <ewalker@ewalkerblog.co.za>
2012-06-23 10:49:09 +0200

    mailer_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,21 @@
 Mystorkparty::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options   = { :host => 'mystorkparty.herokuapp.com/' }
+  
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "ewalker@ewalkerblog.co.za",
+  :authentication => :login,
+  :user_name => "ewalker@ewalkerblog.co.za",
+  :password => "VXLC2fM",
+  }
+  config.cache_classes = false
   # Code is not reloaded between requests
   config.cache_classes = true
 
